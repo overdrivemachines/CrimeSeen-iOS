1df5d8520fbf74bbc8e0ab6ed052ebb52a3b6eec
Dipen Chauhan <get.dipen@gmail.com>
2022-06-04 02:24:24 -0700

    working on tabs in storyboard

---
+++ CrimeSeen/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CrimeSeen
+//
+//  Created by Dipen Chauhan on 6/3/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
